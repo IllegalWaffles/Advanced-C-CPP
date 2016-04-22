944f203122ab82dc76024650963a4273c18e8768
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2016-04-22 12:51:51 -0400

    Started Extra Credit for hw5

---
+++ hw5/TypeBookRecord.cpp
@@ -0,0 +1,45 @@
+#include "TypeBookRecord.h"
+TypeBookRecord::TypeBookRecord()
+{
+
+	TypeBookRecord("","","",0,"");
+
+}
+
+TypeBookRecord::TypeBookRecord(string title, string author, string publisher, int ISBN, string type)
+{
+
+	setTitle(title);
+	setAuthor(author);
+	setPublisher(publisher);
+	setISBN(ISBN);
+	setType(type);
+
+}
+
+void TypeBookRecord::setType(string s){
+
+	type = s;
+
+}
+
+string TypeBookRecord::getType(){
+
+	return type;
+
+}
+
+void TypeBookRecord::print() const{
+
+	BookRecord::print();//Prints all the standard information
+	
+	string tempType;
+	if(type.length() > 15)
+		tempType = type.substr(0,15);
+	else 
+		tempType = type;
+
+	cout.setf(ios::left, ios::adjustfield);
+	cout << " " << setw(15) << tempType;
+
+}
---
+++ hw5/TypeBookRecord.h
@@ -0,0 +1,25 @@
+#ifndef TYPEBOOKRECORD
+#define TYPEBOOKRECORD
+
+#include <string>
+#include "BookRecord.h"
+
+using namespace std;
+
+class TypeBookRecord:public BookRecord{
+
+	public:
+		TypeBookRecord();
+		TypeBookRecord(string, string, string, int, string);
+		
+		void setType(string);
+		string getType();
+		
+		void print() const;
+		
+	private:
+		string type;
+
+
+};
+#endif
