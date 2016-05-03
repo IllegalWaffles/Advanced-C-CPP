e9d8416c97a48e7768449321feed4dca387754d0
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2016-05-02 23:42:12 -0400

    Bacup

--- extra_credit_final/ProgramRunner.cpp
+++ extra_credit_final/ProgramRunner.cpp
@@ -7,32 +7,99 @@
 
 using namespace std;
 
-int main()
-{
+int execute(string);
+void printmenu();
 
-	string theString;
-	pid_t pid = fork();
+int main(){
 
-	if(pid == 0)
-	{
+	//Specifies the path for each program to run
+	const string firstProgram = "/usr/bin/gnome-terminal -e ./testprogram";
+	const string secondProgram = "/usr/lib/firefox/firefox"; 
+	const string thirdProgram = "/usr/lib/gnome-character-map"; 
+	const string fourthProgram = "/usr/bin/gnome-calculator";
 
-		for(int j = 0; j < 10; j++)
-			cout << "Child: " << j << endl;
+	bool finished = false;
+	string cmd;
 
-	}
-	else
-	{
+	printmenu();
 
-		wait(&pid);
-		for(int i = 0; i < 10; ++i)
+	while(!finished){
+		
+		cout << ">";
+		cin >> cmd;
+		
+		if(cmd.compare("1") == 0)
 		{
 
-			cout << "Parent: " << i << endl;;
+			execute(firstProgram);
 
 		}
+		else if(cmd.compare("2") == 0)
+		{
 
-	}
+			execute(secondProgram);
+
+		}	
+		else if(cmd.compare("3") == 0)
+		{
+
+			execute(thirdProgram);
+
+		}
+		else if(cmd.compare("4") == 0)
+		{
+
+			execute(fourthProgram);
+
+		}
+		else if(cmd.compare("5") == 0)
+		{
+	
+			finished = true;
+		
+		}
+
+	}	
+
 
 	return 0;
+
+}
+
+void printmenu(){
+
+	cout << "Menu:" << endl;
+	cout << "1: TestPrgrm (local program)" << endl;
+	cout << "2: FireFox" << endl;
+	cout << "3: Character map or something" << endl;
+	cout << "4: Calculator" << endl;
+	cout << "5: Exit this program" << endl;
+
+}
+
+int execute(string cmd){
+
+	pid_t pid = vfork();
+
+	if(pid == 0){
+
+	//Child process code
+
+
+		/*
+		if(cmd.compare("/usr/bin/gnome-terminal -e ./testprogram") == 0){
+
+			execl("/usr/bin/gnome-terminal", "/usr/bin/gnome-terminal", "-e", "./testprogram", NULL);	
+			
+		}else{
+
+			execl(cmd.c_str(), cmd.c_str(), NULL);
+			
+		}
+		*/
+		system(cmd.c_str());
+		exit(0);//Ends this process
+
+	}
 
 }
---
+++ extra_credit_final/TestPrgrm.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "This is the test program!" << endl;
+	cin.ignore();
+
+	return 0;
+
+}
